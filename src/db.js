8d302bc2807ab06559017722ee77b028b873ed21
Nopdanai Sayamnet <nopdanai1@gmail.com>
2023-04-02 22:40:43 +0700

    feat: write docker file for app and restructure folder

--- src/db.js
+++ src/db.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   console.log("Connecting to MongoDB...");
 
+  const MONGO_USER = process.env.MONGO_USER;
+  const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+  const MONGO_HOST = process.env.MONGO_HOST;
+  const MONGO_PORT = process.env.MONGO_PORT;
+  const URI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?authMechanism=DEFAULT`;
+
   mongoose.set("strictQuery", false);
   await mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(URI)
     .then((res) => {
       console.log(`MongoDB Connected: ${res.connection.host}`);
     })
